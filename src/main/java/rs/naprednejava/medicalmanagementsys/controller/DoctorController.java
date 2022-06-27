5b588e340735e0e7457f7a3251b37858a3c85b02
MilosVu <vujic.milos1998@gmail.com>
2022-06-27 16:23:29 +0200

    doctor controller kreiran

---
+++ src/main/java/rs/naprednejava/medicalmanagementsys/controller/DoctorController.java
@@ -0,0 +1,46 @@
+package rs.naprednejava.medicalmanagementsys.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
+public class DoctorController {
+
+	@Autowired
+    private DoctorService doctorService;
+    
+    //Get all users
+    @GetMapping("/doctors")
+    public List<Doctor> getAllDoctors(){
+        return doctorService.getAllDoctors();
+    }
+    
+    
+    @PostMapping("/doctors")
+ 	public Doctor createDoctor(@RequestBody Doctor doctor) {
+    	return doctorService.createDoctor(doctor);
+ 	}
+    
+    
+    //Get doctor by id
+    @GetMapping("/doctors/{id}")
+ 	public ResponseEntity<Doctor> getUsersById(@PathVariable String id) {
+    	return doctorService.getUsersById(id);
+ 	}
+	
+}
