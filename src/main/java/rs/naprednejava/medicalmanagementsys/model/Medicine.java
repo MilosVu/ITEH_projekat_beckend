6af8dec9299e133f3de617342720490f1d78825d
Dušan Gajić <dg20170149@student.fon.bg.ac.rs>
2022-06-28 00:37:55 +0200

    medicine added

---
+++ src/main/java/rs/naprednejava/medicalmanagementsys/model/Medicine.java
@@ -0,0 +1,71 @@
+package rs.naprednejava.medicalmanagementsys.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "medicine")
+public class Medicine {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long medicineid;
+	
+	@Column(name = "name")
+	private String name;
+
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "company_id", referencedColumnName = "company_id")
+    private PharmaceuticalCompany pharmaceuticalCompany;
+	
+	public Long getMedicineid() {
+		return medicineid;
+	}
+
+	public void setMedicineid(Long medicineid) {
+		this.medicineid = medicineid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public PharmaceuticalCompany getPharmaceuticalCompany() {
+		return pharmaceuticalCompany;
+	}
+
+	public void setName(PharmaceuticalCompany pharmaceuticalCompany) {
+		this.pharmaceuticalCompany = pharmaceuticalCompany;
+	}
+
+	public Medicine(Long medicineid, String name, PharmaceuticalCompany pharmaceuticalCompany) {
+		this.medicineid = medicineid;
+		this.name = name;
+		this.pharmaceuticalCompany = pharmaceuticalCompany;
+	}
+	
+	public Medicine(String name, PharmaceuticalCompany pharmaceuticalCompany) {
+		
+		this.name = name;
+		this.pharmaceuticalCompany = pharmaceuticalCompany;
+	}
+
+	public Medicine() {
+	}
+
+	@Override
+	public String toString() {
+		return "Medicine [medicineid=" + medicineid + ", name=" + name + ", pharmaceuticalCompany="
+				+ pharmaceuticalCompany + "]";
+	}
+}
